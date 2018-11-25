29a877df70e16cc3346b6b043dc35bd509236c67
Francisco Hodge <hello@franciscohodge.com>
2018-11-25 12:58:10 -0500

    Cleanup: Removing unneeded utility function

--- src/lib/services/Utilities.js
+++ src/lib/services/Utilities.js
@@ -1,5 +1,3 @@
-export const getRandomBaseClass = () => `simplekeyboard_id-${Math.random().toString(36).substring(7)}`;
-
 export const parseProps = props => ({
   ...props,
   theme: `simple-keyboard ${(props.theme || "hg-theme-default")}`
