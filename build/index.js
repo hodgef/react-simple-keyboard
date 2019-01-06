0e689341556d40fc06250a20d8ab02e5641c11ee
Francisco Hodge <hello@franciscohodge.com>
2019-01-06 15:54:49 -0500

    Updated devDependencies

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   react-simple-keyboard v1.14.1
+ *   react-simple-keyboard v1.14.2
  *   https://github.com/hodgef/react-simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
