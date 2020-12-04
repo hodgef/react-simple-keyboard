d9e381221c795465be13e4ea7327bdfd1e0d75d9
Francisco Hodge <hello@franciscohodge.com>
2020-12-04 09:27:42 +0000

    Build update

--- build/css/index.css
+++ build/css/index.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   react-simple-keyboard v2.5.77
+ *   react-simple-keyboard v2.5.78
  *   https://github.com/hodgef/react-simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
