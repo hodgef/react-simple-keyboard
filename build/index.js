9fee78d01b4965c5e4c9bbe10435ba0064febe9f
Francisco Hodge <hello@franciscohodge.com>
2021-10-13 08:39:32 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   react-simple-keyboard v3.3.0
+ *   react-simple-keyboard v3.3.1
  *   https://github.com/hodgef/react-simple-keyboard
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
