8d2cbe128c7d6c7042a5d32fb2b8e9e5a8d66d48
Francisco Hodge <hello@franciscohodge.com>
2021-10-21 08:06:34 +0000

    Build update

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,27 @@
 module.exports = {
   presets: [
     "@babel/preset-react",
-    ["@babel/env", {
-      corejs:"3",
-      useBuiltIns: 'entry',
-      "targets": {
-        "browsers": [
-          "edge >= 16",
-          "safari >= 9",
-          "firefox >= 57",
-          "ie >= 11",
-          "ios >= 9",
-          "chrome >= 49"
-        ]
+    [
+      "@babel/env",
+      {
+        corejs: "3",
+        useBuiltIns: "entry",
+        targets: {
+          browsers: [
+            "edge >= 16",
+            "safari >= 9",
+            "firefox >= 57",
+            "ie >= 11",
+            "ios >= 9",
+            "chrome >= 49",
+          ],
+        },
       },
-    }],
-    "@babel/preset-typescript"
+    ],
+    "@babel/preset-typescript",
   ],
   plugins: [
     ["@babel/plugin-proposal-class-properties"],
-    ["@babel/plugin-transform-typescript"]
-  ]
+    ["@babel/plugin-transform-typescript"],
+  ],
 };
