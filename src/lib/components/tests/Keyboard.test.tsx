71c6170a3fa9ab5362254664fcb8dcd597ddbfbd
Francisco Hodge <hello@franciscohodge.com>
2021-11-27 01:24:20 -0800

    Update enzyme adapter

--- src/lib/components/tests/Keyboard.test.tsx
+++ src/lib/components/tests/Keyboard.test.tsx
@@ -1,5 +1,5 @@
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import * as React from "react";
 
 import { setDOM } from "../../../utils/TestUtility";
