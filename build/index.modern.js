29c5ed16c8e3091450d9406bf69e6cfd2c5ae4ad
Francisco Hodge <hello@franciscohodge.com>
2024-05-24 08:24:24 +0000

    Build update

--- build/index.modern.js
+++ build/index.modern.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   react-simple-keyboard v3.7.122 (index.modern.js - Modern Browsers bundle)
+ *   react-simple-keyboard v3.7.123 (index.modern.js - Modern Browsers bundle)
  *   https://github.com/hodgef/react-simple-keyboard
  *
  *   NOTE: This modern browsers bundle (index.modern.js) removes all polyfills
